8eaa1b6d24370b7454425e3482e389a16110ceba
khue <kv242821@gmail.com>
2023-07-09 14:00:19 +0700

    add base model & controller

--- utils/catchAsync.js
+++ utils/catchAsync.js
@@ -1,3 +1,13 @@
+// import {Request, Response, NextFunction} from 'express';
+
+// const catchAsync = (fn: Function) => {
+//   return (req: Request, res: Response, next: NextFunction) => {
+//     fn(req, res, next).catch((err: Error) => next(err));
+//   };
+// };
+
+// export {catchAsync};
+
 const catchAsync = (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch((err) => next(err));
--- utils/filterObj.js
+++ utils/filterObj.js
@@ -1,3 +1,18 @@
+// const filterObj = <T extends object>(
+//   obj: T,
+//   ...allowedFields: Array<keyof T>
+// ): Partial<T> => {
+//   const newObj: Partial<T> = {};
+//   Object.keys(obj).forEach((el) => {
+//     if (allowedFields.includes(el as keyof T)) {
+//       newObj[el as keyof T] = obj[el as keyof T];
+//     }
+//   });
+//   return newObj;
+// };
+
+// export { filterObj };
+
 const filterObj = (obj, ...allowedFields) => {
   const newObj = {};
   Object.keys(obj).forEach((el) => {

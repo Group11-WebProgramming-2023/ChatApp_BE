23ea1a0724401bbd8078868db0cf904777d5c31f
khue <kv242821@gmail.com>
2023-07-14 23:40:34 +0700

    update user api

--- routes/user.js
+++ routes/user.js
@@ -16,13 +16,17 @@ router.get(
 router.get("/get-me", authController.protect, userController.getMe);
 router.patch("/update-me", authController.protect, userController.updateMe);
 router.get(
-  "/get-all-verified-users",
+  "/all-verified-users",
   authController.protect,
   userController.getAllVerifiedUsers
 );
-router.get("/get-users", authController.protect, userController.getUsers);
-router.get("/get-requests", authController.protect, userController.getRequests);
-router.get("/get-friends", authController.protect, userController.getFriends);
+router.get("/all-users", authController.protect, userController.getUsers);
+router.get(
+  "/all-friend-requests",
+  authController.protect,
+  userController.getRequests
+);
+router.get("/all-friends", authController.protect, userController.getFriends);
 
 router.post(
   "/start-audio-call",
